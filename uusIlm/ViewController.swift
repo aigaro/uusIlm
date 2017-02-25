89c05c45695d9bf1e4c05804cb7637be0d1e5227
Apple Inc <aigar@4d6a-fee3-8aff-dacb-9d80-87fc-07d0-2001.dyn.estpak.ee>
2017-02-25 16:36:18 +0200

    Tahaks, et nupuvajutus värskendaks

--- uusIlm/ViewController.swift
+++ uusIlm/ViewController.swift
@@ -17,20 +17,23 @@ var ilm = ilmaAndmed()
 
 class ViewController: UIViewController {
     
+    var kuupaev = 0
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         Alamofire.request("https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php").responseString { response in
             let xmltest = SWXMLHash.parse(response.data!)
             //kuupäevi saab muuta [0] nendes sulgudes
+
             
-            
-            
-            ilm.tempMin_day = Int(xmltest["forecasts"]["forecast"][0]["day"]["tempmin"].element!.text!)!
-            ilm.tempMax_day = Int(xmltest["forecasts"]["forecast"][0]["day"]["tempmax"].element!.text!)!
-            ilm.ilmTekst_day = xmltest["forecasts"]["forecast"][0]["day"]["text"].element!.text!
+            ilm.tempMin_day = Int(xmltest["forecasts"]["forecast"][1]["day"]["tempmin"].element!.text!)!
+            ilm.tempMax_day = Int(xmltest["forecasts"]["forecast"][1]["day"]["tempmax"].element!.text!)!
+            ilm.ilmTekst_day = xmltest["forecasts"]["forecast"][1]["day"]["text"].element!.text!
             
             ilm.tempMin_night = Int(xmltest["forecasts"]["forecast"][0]["night"]["tempmin"].element!.text!)!
             ilm.tempMax_night = Int(xmltest["forecasts"]["forecast"][0]["night"]["tempmax"].element!.text!)!
@@ -41,7 +44,7 @@ class ViewController: UIViewController {
             for i in 0...3 {
             ilm.weather_date.append(xmltest["forecasts"]["forecast"][i].element!.attribute(by: "date")!.text)
             }
-            print(ilm.weather_date[0])
+            print(ilm.weather_date[self.kuupaev])
             
             self.Date_1_Button.setTitle(ilm.weather_date[0], for: .normal)
             self.Date_2_Button.setTitle(ilm.weather_date[1], for: .normal)
@@ -127,16 +130,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var Date_4_Button: UIButton!
     
     @IBAction func Nupuvajutus(_ sender: UIButton) {
-        var kuupaev = Int()
+        
         switch sender.tag {
         case 0:
             kuupaev = 0
+            viewDidLoad()
         case 1:
             kuupaev = 1
+            viewDidLoad()
         case 2:
             kuupaev = 2
+            viewDidLoad()
         case 3:
             kuupaev = 3
+            viewDidLoad()
         default:
             break
         }
